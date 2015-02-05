528df50c9126ed0a9c0bde8274e91ad2cfe8b1e1
Bryce Mecum <petridish@gmail.com>
2015-02-04 16:54:34 -0900

    Change extrudeBy to match attribute table (ELEV)

--- convert.js
+++ convert.js
@@ -6,7 +6,7 @@ shp2stl.shp2stl(file,
         width: 100, //in STL arbitrary units, but typically 3D printers use mm
         height: 10,
         extraBaseHeight: 0,
-        extrudeBy: "Pop_psmi",
+        extrudeBy: "ELEV",
         simplification: .8,
         binary: true,
         cutoutHoles: false,
