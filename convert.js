71b676d3f07989deeda2f05fc57bee063eb4bdbf
Bryce Mecum <petridish@gmail.com>
2015-02-04 16:03:36 -0900

    Initial commit

---
+++ convert.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var shp2stl = require('shp2stl');
+var file = 'douglas-contours-500.shp';
+shp2stl.shp2stl(file,
+    {
+        width: 100, //in STL arbitrary units, but typically 3D printers use mm
+        height: 10,
+        extraBaseHeight: 0,
+        extrudeBy: "Pop_psmi",
+        simplification: .8,
+        binary: true,
+        cutoutHoles: false,
+        verbose: true,
+        extrusionMode: 'straight'
+    },
+    function(err, stl) {
+        fs.writeFileSync('douglas-contours-500.stl',  stl);
+    }
+);
